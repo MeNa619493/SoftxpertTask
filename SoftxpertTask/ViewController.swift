3fa159a0ff68827d6650af7ba7e127136e9a6637
Mina Ezzat <53598947+MeNa619493@users.noreply.github.com>
2022-09-19 00:24:01 -0700

    Initial Commit

---
+++ SoftxpertTask/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SoftxpertTask
+//
+//  Created by Mina Ezzat on 9/19/22.
+//  Copyright © 2022 Mina Ezzat. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
